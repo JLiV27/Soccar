2e4da182253571a02ff1f11ae64d7879d18dc159
jliv27 <jace_livigni27@milton.edu>
2025-02-05 20:21:33 -0500

    Initial commit

---
+++ src/BasicGameApp.java
@@ -0,0 +1,195 @@
+//Basic Game Application
+// Basic Object, Image, Movement
+// Threaded
+
+//*******************************************************************************
+//Import Section
+//Add Java libraries needed for the game
+//import java.awt.Canvas;
+
+//Graphics Libraries
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+//*******************************************************************************
+
+public class BasicGameApp implements Runnable {
+
+    Vehicle Car1;
+    Vehicle Car2;
+    Vehicle Ball;
+
+    boolean isColliding1 = false;
+    boolean isColliding2 = false;
+    boolean isColliding3 = false;
+
+    Image BackgroundPic;
+
+    //Variable Definition Section
+    //Declare the variables used in the program
+    //You can set their initial values too
+
+    //Sets the width and height of the program window
+    final int WIDTH = 1000;
+    final int HEIGHT = 1000;
+
+    //Declare the variables needed for the graphics
+    public JFrame frame;
+    public Canvas canvas;
+    public JPanel panel;
+
+    public BufferStrategy bufferStrategy;
+
+    // Main method definition
+    // This is the code that runs first and automatically
+    public static void main(String[] args) {
+        BasicGameApp ex = new BasicGameApp();   //creates a new instance of the game
+        new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method
+    }
+
+
+    // This section is the setup portion of the program
+    // Initialize your variables and construct your program objects here.
+    public BasicGameApp() { // BasicGameApp constructor
+
+        setUpGraphics();
+
+        //variable and objects
+        //create (construct) the objects needed for the game
+        Car1 = new Vehicle(0,400,5,3,150,100);
+        Car1.pic = Toolkit.getDefaultToolkit().getImage("Car1R.png");
+
+        Car2 = new Vehicle(850,400,-4,-3,150,100);
+        Car2.pic = Toolkit.getDefaultToolkit().getImage("Car2L.png");
+
+        Ball = new Vehicle(450,400,1,1,100,100);
+        Ball.pic = Toolkit.getDefaultToolkit().getImage("Ball.png");
+
+        BackgroundPic = Toolkit.getDefaultToolkit().getImage("Field.png");
+
+    } // end BasicGameApp constructor
+
+
+//*******************************************************************************
+//User Method Section
+//
+// put your code to do things here.
+
+    // main thread
+    // this is the code that plays the game after you set things up
+    public void run() {
+        //for the moment we will loop things forever.
+        while (true) {
+            moveThings();  //move all the game objects
+            Collide();
+            render();  // paint the graphics
+            pause(10); // sleep for 10 ms
+        }
+    }
+
+    public void moveThings() {
+        //call the move() code for each object
+        Car1.move();
+        Car2.move();
+        Ball.move();
+    }
+
+    public void Collide(){
+        if(Car1.hitbox.intersects(Ball.hitbox) && !isColliding1){
+            Ball.dx = -Ball.dx;
+            Ball.dy = -Ball.dy;
+            isColliding1 = true;
+        }
+        if(Car1.hitbox.intersects(Car2.hitbox) && !isColliding2){
+            Car1.dx = -Car1.dx;
+            Car1.dy = -Car1.dy;
+            Car2.dx = -Car2.dx;
+            Car2.dy = -Car2.dy;
+            isColliding2 = true;
+        }
+        if(Car2.hitbox.intersects(Ball.hitbox) && !isColliding3){
+            Ball.dx = -Ball.dx;
+            Ball.dy = -Ball.dy;
+            isColliding3 = true;
+        }
+
+        if(!Car1.hitbox.intersects(Ball.hitbox)){
+            isColliding1 = false;
+        }
+        if(!Car1.hitbox.intersects(Car2.hitbox)){
+            isColliding2 = false;
+        }
+        if(!Car2.hitbox.intersects(Ball.hitbox)){
+            isColliding3 = false;
+        }
+    }
+
+
+    //Paints things on the screen using bufferStrategy
+    private void render() {
+        Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+
+        g.drawImage(BackgroundPic, 0,0,WIDTH,HEIGHT,null);
+        g.drawImage(Car1.pic, Car1.xpos, Car1.ypos, Car1.width, Car1.height, null);
+        g.drawImage(Car2.pic, Car2.xpos, Car2.ypos, Car2.width, Car2.height, null);
+        g.drawImage(Ball.pic, Ball.xpos, Ball.ypos, Ball.width, Ball.height, null);
+        //draw the images
+        g.drawRect(Car1.hitboxL.x,Car1.hitboxL.y,Car1.hitboxL.width,Car1.hitboxL.height);
+        g.drawRect(Car1.hitboxR.x,Car1.hitboxR.y,Car1.hitboxR.width,Car1.hitboxR.height);
+        g.drawRect(Car1.hitboxT.x,Car1.hitboxT.y,Car1.hitboxT.width,Car1.hitboxT.height);
+        g.drawRect(Car1.hitboxB.x,Car1.hitboxB.y,Car1.hitboxB.width,Car1.hitboxB.height);
+
+        g.drawRect(Car2.hitboxL.x,Car2.hitboxL.y,Car2.hitboxL.width,Car2.hitboxL.height);
+        g.drawRect(Car2.hitboxR.x,Car2.hitboxR.y,Car2.hitboxR.width,Car2.hitboxR.height);
+        g.drawRect(Car2.hitboxT.x,Car2.hitboxT.y,Car2.hitboxT.width,Car2.hitboxT.height);
+        g.drawRect(Car2.hitboxB.x,Car2.hitboxB.y,Car2.hitboxB.width,Car2.hitboxB.height);
+
+        g.dispose();
+        bufferStrategy.show();
+    }
+
+    //Pauses or sleeps the computer for the amount specified in milliseconds
+    public void pause(int time ) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    //Graphics setup method
+    private void setUpGraphics() {
+        frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
+
+        panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
+        panel.setLayout(null);   //set the layout
+
+        // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
+        // and trap input events (Mouse and Keyboard events)
+        canvas = new Canvas();
+        canvas.setBounds(0, 0, WIDTH, HEIGHT);
+        canvas.setIgnoreRepaint(true);
+
+        panel.add(canvas);  // adds the canvas to the panel.
+
+        // frame operations
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
+        frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
+        frame.setResizable(false);   //makes it so the frame cannot be resized
+        frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
+        /*JFrame jf = new JFrame();
+        jf.setSize(1920,1080);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);*/
+
+        // sets up things so the screen displays images nicely.
+        canvas.createBufferStrategy(2);
+        bufferStrategy = canvas.getBufferStrategy();
+        canvas.requestFocus();
+        System.out.println("DONE graphic setup");
+    }
+
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,49 @@
+import java.awt.*;
+
+public class Vehicle {
+    public Image pic;
+    public int xpos;
+    public int ypos;
+    public int dx = 3;
+    public int dy = 3;
+    public int width = 50;
+    public int height = 50;
+    public Rectangle hitbox;
+    public Rectangle hitboxL;
+    public Rectangle hitboxR;
+    public Rectangle hitboxT;
+    public Rectangle hitboxB;
+
+    public Vehicle(int paramXpos, int paramYpos, int paramDx, int paramDy, int paramWidth, int paramHeight) {
+        xpos = paramXpos;
+        ypos = paramYpos;
+        dx = paramDx;
+        dy = paramDy;
+        width = paramWidth;
+        height = paramHeight;
+
+        hitboxL = new Rectangle(xpos, ypos, width, height);
+        hitboxR = new Rectangle(xpos, ypos, width, height);
+        hitboxT = new Rectangle(xpos, ypos, width, height);
+        hitboxB = new Rectangle(xpos, ypos, width, height);
+    }
+
+    public void move() {
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+
+        if (ypos >= 700 - width || ypos <= 0) {
+            dy = -dy;
+        }
+
+        if (xpos >= 1000 - width || xpos <= 0) {
+            dx = -dx;
+        }
+        hitboxL = new Rectangle(xpos, ypos, width, height);
+        hitboxR = new Rectangle(xpos, ypos, width, height);
+        hitboxT = new Rectangle(xpos, ypos, width, height);
+        hitboxB = new Rectangle(xpos, ypos, width, height);
+    }
+}
+
+
